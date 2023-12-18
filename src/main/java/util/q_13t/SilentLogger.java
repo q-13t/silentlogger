bebe5371c1919acc93325abba84d59220769bc00
q-13t <vova29092002@gmail.com>
2023-12-18 17:03:36 +0200

    IMP: FileWriterThread [WIP]

--- src/main/java/util/q_13t/SilentLogger.java
+++ src/main/java/util/q_13t/SilentLogger.java
@@ -1,10 +1,51 @@
 package util.q_13t;
 
+import java.util.Queue;
+import java.util.LinkedList;
 import java.time.LocalDateTime;
 
 public class SilentLogger {
-    private static final String GENERIC_FORMAT = "%s\t%c [%s]\t\t\t: %s";
+    private static String GENERIC_FORMAT = "%s\t%c [%s]\t\t\t: %s";
     private String className = "";
+    private static Queue<String> logQueue = new LinkedList<String>();
+    private boolean logToFile = false;
+    private static Object FileWriterLock = new Object();
+    private FileWriterThread FWT = new FileWriterThread();
+
+    static class FileWriterThread extends Thread {
+
+        private static boolean threadIsAlive = true;
+
+        public static void stopThread() {
+            synchronized (FileWriterLock) {
+                threadIsAlive = false;
+                FileWriterLock.notify();
+            }
+        }
+
+        public FileWriterThread() {
+            setDaemon(true);
+        }
+
+        @Override
+        public void run() {
+            synchronized (FileWriterLock) {
+                try {
+                    while (threadIsAlive) {
+                        if (!logQueue.isEmpty()) {
+                            System.out.println("*Writing To File Imitation*\t\t" + logQueue.poll());
+                        }
+                        if (logQueue.isEmpty())
+                            FileWriterLock.wait();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println("FileWriterThread Thread Stopped");
+            }
+        }
+
+    }
 
     private SilentLogger(Class<?> name) {
         setClassName(name);
@@ -12,6 +53,22 @@ public class SilentLogger {
 
     private void setClassName(Class<?> name) {
         className = name.getSimpleName();
+    }
+
+    public static void setFormat(String format) {
+        GENERIC_FORMAT = format;
+    }
+
+    public void setLogToFile(boolean val) {
+        logToFile = val;
+        FWT.start();
+    }
+
+    private void informFileWriter(String message) {
+        synchronized (FileWriterLock) {
+            logQueue.add(message);
+            FileWriterLock.notify();
+        }
     }
 
     public static SilentLogger getLogger(Class<?> name) {
@@ -38,5 +95,12 @@ public class SilentLogger {
             debug = String.format(GENERIC_FORMAT, LocalDateTime.now(), 'D', className, message);
         }
         System.out.println(debug);
+        if (logToFile)
+            informFileWriter(debug);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        FileWriterThread.stopThread();
     }
 }
