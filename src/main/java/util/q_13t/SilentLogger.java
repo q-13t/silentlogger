36aa15c441c2d3afc768997fbeca982fb32b8378
q-13t <vova29092002@gmail.com>
2023-12-18 15:52:56 +0200

    IMP: Basic Debug

--- src/main/java/util/q_13t/SilentLogger.java
+++ src/main/java/util/q_13t/SilentLogger.java
@@ -1,24 +1,42 @@
 package util.q_13t;
 
-import java.sql.Time;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class SilentLogger {
+    private static final String GENERIC_FORMAT = "%s\t%c [%s]\t\t\t: %s";
     private String className = "";
 
-    public SilentLogger(Class<?> name) {
-        if (name != null) {
-            className = name.getSimpleName();
+    private SilentLogger(Class<?> name) {
+        setClassName(name);
+    }
+
+    private void setClassName(Class<?> name) {
+        className = name.getSimpleName();
+    }
+
+    public static SilentLogger getLogger(Class<?> name) {
+        if (name == null) {
+            SilentLogger silentLogger = new SilentLogger(SilentLogger.class);
+            silentLogger.debug("NULL Value Passed Logger Will Refer To Self");
+            return silentLogger;
         } else {
-            debug("NULL Value Passed");
+            return new SilentLogger(name);
         }
     }
 
+    public static SilentLogger getLogger() {
+        SilentLogger silentLogger = new SilentLogger(SilentLogger.class);
+        silentLogger.debug("NULL Value Passed Logger Will Refer To Self");
+        return silentLogger;
+    }
+
     public void debug(String message) {
+        String debug;
         if (className.isEmpty()) {
-            System.out.println(new Time(new Date().getTime()) + " D SilentLogger: " + message);
+            debug = String.format(GENERIC_FORMAT, LocalDateTime.now(), 'D', getClass().getSimpleName(), message);
         } else {
-            System.out.println(new Time(new Date().getTime()) + " D " + className + ": " + message);
+            debug = String.format(GENERIC_FORMAT, LocalDateTime.now(), 'D', className, message);
         }
+        System.out.println(debug);
     }
 }
---
+++ src/test/java/util/q_13t/SilentLoggerTest.java
@@ -0,0 +1,31 @@
+package util.q_13t;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SilentLoggerTest {
+    // private static final ByteArrayOutputStream outContent = new
+    // ByteArrayOutputStream();
+    // private static final ByteArrayOutputStream errContent = new
+    // ByteArrayOutputStream();
+    // private static final PrintStream originalOut = System.out;
+    // private static final PrintStream originalErr = System.err;
+
+    // @BeforeAll
+    // public static void setUpStreams() {
+    // System.setOut(new PrintStream(outContent));
+    // System.setErr(new PrintStream(errContent));
+    // }
+
+    // @AfterAll
+    // public static void restoreStreams() {
+    // System.setOut(originalOut);
+    // System.setErr(originalErr);
+    // }
+
+}
